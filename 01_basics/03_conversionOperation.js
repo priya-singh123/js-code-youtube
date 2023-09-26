9df349fbb2fd9135401e5d58391c95abd5dee89d
priya-singh123 <77691637+priya-singh123@users.noreply.github.com>
2023-09-26 19:47:50 +0000

    03_ConversionOperation.js

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,16 +1,16 @@
 let score ="33abh"
 
-// console.log(typeof score);
-// console.log(typeof(score))
+console.log(typeof score);
+console.log(typeof(score))
 
 let valueInNumber = Number(score)
-// console.log(typeof valueInNumber);
-// console.log(valueInNumber)
+console.log(typeof valueInNumber);
+console.log(valueInNumber)
 
-//"33" =>33
-//"33abc" =>NaN
-//true=>1
-//false=>0
+"33" =>33
+"33abc" =>NaN
+true=>1
+false=>0
 
 let isLoggedIn =1
 let bolleanIsLoggedIn =Boolean(isLoggedIn)
